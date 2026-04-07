34dfcd3c0eec62b63aaa27b5c0154ee1d8d9f13a
Toni <aabm00@gmail.com>
2026-04-07 10:29:46 +0200

    Componentes, props y variables reactivas

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,18 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+
+import {ref} from 'vue'
+import ShoppingCart from './components/ShoppingCart.vue';
+
+const product = ref({
+  name: "Camiseta",
+  quantity: 10
+})
+</script>
 
 <template>
   <div>
-    <h1>Hola Mundo</h1>
+    <h1>Mi carrito de compras</h1>
+
+    <ShoppingCart v-bind="product" />
   </div>
 </template>
---
+++ src/components/ShoppingCart.vue
@@ -0,0 +1,33 @@
+<template>
+    <div>
+        <span>{{ name }}</span>
+        <span>{{ quantity }}</span>
+    </div>
+</template>
+
+<script setup lang="ts">
+defineProps<{
+  name: string;
+  quantity: number;
+}>()
+
+
+</script>
+
+<style scoped>
+  div {
+    display: flex;
+    gap: 10px;
+    align-items: center;
+    margin-bottom: 10px;
+  }
+
+  button {
+    padding: 10px 20px;
+  }
+
+  span {
+    width: 70px;
+  }
+
+</style>
